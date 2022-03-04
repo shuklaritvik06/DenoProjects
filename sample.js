b9d48b799e21fc1eea3c7a61c25ce614b421d0b2
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-03-05 01:24:06 +0530

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,9 @@
+const name = "Ramesh";
+const myDetails = {address: "Rajeshnagar",age:35};
+console.log(name);
+console.log(myDetails);
+console.table([1,2,3,4,6]);
+console.table({naam:"Rakesh",height:5});
+
+
+// Run `deno run fileName`
